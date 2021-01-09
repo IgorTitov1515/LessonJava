2c6132f549b36d1ced3345ab6650b6dd8dc76c0f
Titovigor1515@gmail.com <Titovigor1515@gmail.com>
2021-01-09 15:08:17 +0200

    Lesson1

---
+++ src/main/java/Test3.java
@@ -0,0 +1,12 @@
+public class Test3 {
+    public static void main(String[] args) {
+        int a = 3;
+        for(int i = 1; i <=10; i++ ){
+          //  System.out.println(a * i);
+           // System.out.println(String.format("a * i =", a * i ));
+           // System.out.println(a,*, i,a);
+           /* int b = a*i;
+            System.out.println(String.format(a, "*", i, "=", b));*/
+        }
+    }
+}
